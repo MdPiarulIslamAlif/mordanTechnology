b99a225a16943f9f89337a497392d93c3c50b8e9
MdPiarulIslamAlif <mdpiarulislamalif7@gmial.com>
2023-12-22 10:57:47 +0600

    added a router nad show a navbar in ui fix some provlem

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import Navbar from "../../Component/Navbar/Navbar";
 
 const Home = () => {
     return (
         <div>
+            <Navbar />
             <h1> this home page
+
                 
             </h1>
         </div>
